fbf03da54b280bd0f3a1d4d46380099be1b06044
Gage Moore <ggemre+github@proton.me>
2022-11-29 20:53:49 -0700

    Add delete button

--- react/frontend/src/App.js
+++ react/frontend/src/App.js
@@ -2,17 +2,36 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [name, setName] = useState("");
-  const [temp, setTemp] = useState("");
+  const [idea, setIdea] = useState('');
+  const [ideas, setIdeas] = useState([]);
+
+  const addIdea = idea => {
+    setIdeas([...ideas, idea]);
+  };
+
+  const deleteIdea = index => {
+    const newIdeas = [...ideas];
+    newIdeas.splice(index, 1);
+    setIdeas(newIdeas);
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
       <h1>My React App</h1>
-      <input type="text" placeholder="Enter your name" onChange={ e => setName(e.target.value) } />
-      <p>{ name ? "Enter a temperature below, " + name + "." : "Type your name above." }</p>
 
-      <input type="number" placeholder="Enter a temperature in F" onChange={ e => setTemp(e.target.value) } />
-      <p>{ temp ? "The temperature is " + ((parseFloat(temp) - 32) / 1.8).toFixed(2)  + " C." : "Type a temperature in F above." }</p>
+      <input type='text' placeholder='Enter an idea' onChange={ e => setIdea(e.target.value) } />
+      <button onClick={ () => addIdea(idea) }>Add Idea</button>
+      <ul>
+      { ideas.map((idea, index) => (
+        <li>
+          { idea }
+          <button onClick={ () => deleteIdea(index) }>Delete</button>
+        </li>
+      )) }
+      </ul>
+
+      <p>{ ideas.length } awesome ideas.</p>
+      
     </div>
   );
 }
