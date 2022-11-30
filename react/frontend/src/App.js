25caec224ac543f57cd829b137b2586b2165c21b
Gage Moore <ggemre+github@proton.me>
2022-11-29 20:33:00 -0700

    Add react functionality

--- react/frontend/src/App.js
+++ react/frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("");
+  const [temp, setTemp] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My React App</h1>
+      <input type="text" placeholder="Enter your name" onChange={ e => setName(e.target.value) } />
+      <p>{ name ? "Enter a temperature below, " + name + "." : "Type your name above." }</p>
+
+      <input type="number" placeholder="Enter a temperature in F" onChange={ e => setTemp(e.target.value) } />
+      <p>{ temp ? "The temperature is " + ((parseFloat(temp) - 32) / 1.8).toFixed(2)  + " C." : "Type a temperature in F above." }</p>
     </div>
   );
 }
