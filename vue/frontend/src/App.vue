be8ef947db6b16fd85474dcb276e47d411c98263
Gage Moore <ggemre+github@proton.me>
2022-11-29 21:07:14 -0700

    Add vue functionality

--- vue/frontend/src/App.vue
+++ vue/frontend/src/App.vue
@@ -1,31 +1,26 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const idea = ref('')
+const ideas = ref([])
+
+const deleteIdea = index => {
+  ideas.value.splice(index, 1)
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <h1>My Vue App</h1>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+  <input type='text' v-model='idea' />
+  <button @click='ideas.push(idea)'>Add</button>
+  <ul>
+    <li v-for='idea in ideas' :key='idea'>
+      {{ idea }}
+      <button @click='deleteIdea(index)'>Delete</button>
+    </li>
+  </ul>
+
+  <p>{{ ideas.length }} awesome ideas.</p>
+
+</template>
