3db853db53eb444bc8113c51e54da5f84ea2a264
Gage Moore <ggemre+github@proton.me>
2022-12-03 10:19:53 -0700

    Add svelte app

---
+++ svelte/frontend/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  var idea = ''
+  var ideas = []
+</script>
+
+<main>
+  <h1>My Svelte App</h1>
+
+  <input type='text' bind:value={ idea } />
+  <button on:click={ () => ideas = [...ideas, idea] }>Add</button>
+
+  <ul>
+    { #each ideas as idea }
+      <li>{ idea } <button on:click={ () => ideas = ideas.filter((e, i) => ideas.indexOf(idea) != i) }>Delete</button></li>
+    { /each }
+  </ul>
+
+  <p>{ ideas.length } awesome ideas.</p>
+
+</main>
+
+<style>
+
+</style>
